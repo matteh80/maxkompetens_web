b8a2a0cf0c5e2864958abca8dc8b3bf9cb7a860b
Mathias Hedström <hedstrom.mathias@gmail.com>
2016-11-04 11:49:46 +0100

    …

---
+++ web/app/themes/maxkomp_theme/templates/cv/tab-employments.php
@@ -0,0 +1,1 @@
+<button class="btn btn-primary">Lägg till anställning</button>
--- web/app/themes/maxkomp_theme/templates/profile-cv.php
+++ web/app/themes/maxkomp_theme/templates/profile-cv.php
@@ -1,22 +1,34 @@
 <div id="cv-nav">
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Info</a>
+            <a class="nav-link active" data-toggle="tab" href="#info" role="tab">Info</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Befattningar</a>
+            <a class="nav-link" data-toggle="tab" href="#befattningar" role="tab">Befattningar</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Utbildningar</a>
+            <a class="nav-link" data-toggle="tab" href="#utbildningar" role="tab">Utbildningar</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Anställningar</a>
+            <a class="nav-link" data-toggle="tab" href="#employments" role="tab">Anställningar</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Kompetenser</a>
+            <a class="nav-link" data-toggle="tab" href="#info" role="tab">Kompetenser</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Övrigt</a>
+            <a class="nav-link" data-toggle="tab" href="#info" role="tab">Övrigt</a>
         </li>
     </ul>
 </div>
+
+<div class="col-xs-12">
+    <div class="tab-content">
+        <div class="tab-pane active" id="info" role="tabpanel">
+            sadasd
+        </div>
+        <div class="tab-pane" id="befattningar" role="tabpanel">...</div>
+        <div class="tab-pane" id="utbildningar" role="tabpanel">...</div>
+        <div class="tab-pane" id="employments" role="tabpanel"><?php get_template_part('templates/cv/tab', 'employments'); ?></div>
+    </div>
+</div>
+
